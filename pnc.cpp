8250c1c83b6d14d67b159e25fa6f705b3e68145c
anshsemwal <161868466+anshsemwal@users.noreply.github.com>
2025-02-15 03:02:05 +0530

    Create pnc.cpp

---
+++ pnc.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int num) {
+    if (num < 2) return false;
+    for (int i = 2; i * i <= num; i++)
+        if (num % i == 0) return false;
+    return true;
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    cout << (isPrime(num) ? "Prime" : "Not Prime") << endl;
+    return 0;
+}
